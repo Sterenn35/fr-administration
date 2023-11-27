2d5fa734d2f7a13b18303cd3897a2b064503b8a8
Sterenn <slh35@hotmail.fr>
2023-11-27 18:15:16 +0100

    Add module auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,14 @@ import { AssociationsModule } from './associations/associations.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Association } from './associations/association.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
               type: 'sqlite',
               database: 'mydatabase.db',
               entities: [User, Association],
-              synchronize: true}),UsersModule, AssociationsModule],
+              synchronize: true}),UsersModule, AssociationsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService]
 })
